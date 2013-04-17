8db5916e6a18cbc27d547814d5bf6a53ceba0d10
OpenMEngine@gmail.com <OpenMEngine@gmail.com@4b961d98-54b9-7530-f0ad-e76f8faa386a>
2013-04-17 07:56:55 +0000

    Thread pool should be fully functionnal now, they can be paused and unpaused

--- OpenMe/trunk/OpenMe/include/threadPool.h
+++ OpenMe/trunk/OpenMe/include/threadPool.h
@@ -20,7 +20,6 @@ extern "C" {
 
 #include "utils.h"
 #include "queue.h"
-#include "dstring.h"
 #include <pthread.h>
 
 
@@ -44,38 +43,71 @@ typedef struct omeTask
 } omeTask;
 
 
+typedef enum omeThreadPoolStatus
+{
+    OME_THREAD_POOL_STATUS_RUNNING,     // nominal state
+    OME_THREAD_POOL_STATUS_FLUSHING,    // no more tasks can be added while flushing
+    OME_THREAD_POOL_STATUS_TERMINATING, // no more tasks will EVER be added
+    OME_THREAD_POOL_STATUS_PAUSED       // no tasks are dequeued while paused
+} omeThreadPoolStatus;
+
+
 typedef struct omeThreadPool
 {
-    int maxThreads;
-    pthread_t *threads;
-    int maxTasks;
-    omeQueue *waitingTasks;
-    omeTask **runningTasks;
-    int nbRunningTasks;
-    void **tasksData;
-    void *context;
-    pthread_cond_t condEmpty;
-    pthread_cond_t condNotEmpty;
-    pthread_mutex_t mutex;
-    pthread_barrier_t barrier;
-    int lastStarted;
-    int lastId;
-    omeBool terminated;
+    omeThreadPoolStatus status;     // thread pool internal state
+
+    // threads
+    int nbThreads;                  // number of threads in the pool
+    pthread_t *threads;             // threads ids
+
+    // tasks
+    omeQueue *waitingTasks;         // waiting queue holding the next tasks to be processed
+    int nbRunningTasks;             // number of tasks currently running
+    omeTask **runningTasks;         // array of running tasks
+    void *context;                  // user context, transmitted to task
+    int lastStarted;                // id of the last started task
+    int lastId;                     // last delivered id
+
+    // sync stuff
+    pthread_cond_t condEmpty;       // use to wait for an empty waiting queue
+    pthread_cond_t condNotEmpty;    // use to wait for at least one task in the waiting queue
+    pthread_cond_t condNotFull;     // use to wait for an available slot in the waiting queue
+    pthread_cond_t condPause;       // use to wait for the pool to be unpaused
+    pthread_mutex_t mutex;          // main mutex, used for all critical sections
+    pthread_barrier_t barrier;      // used to start threads once the thread pool is fully created
 } omeThreadPool;
 
 
+// Constructor and destructor of a task TODO: private???
 omeTask *omeTaskCreate(omeThreadPoolProcessTask func, void *arg, int id);
 void omeTaskDestroy(omeTask **t);
 
-omeThreadPool *omeThreadPoolCreate(int maxThreads, int maxTasks, void *context);
+// Constructor and destructor of a thread pool
+omeThreadPool *omeThreadPoolCreate(int nbThreads, int maxTasks, void *context);
 void omeThreadPoolDestroy(omeThreadPool **tp);
+
 // TODO: provide a callback to notify the caller of the task status
+// add a task to the pool and returns, it will be processed later
+// return a task id
 omeStatus omeThreadPoolAddTask(omeThreadPool *tp, omeThreadPoolProcessTask func, void *arg, int *id);
+
+// require the status of a task associated with an id, see omeTaskStatus
 omeTaskStatus omeThreadPoolGetTaskStatus(omeThreadPool *tp, int taskId);
+
+// wait for the completion of all waiting tasks then returns
+// no tasks can be added while flushing
+omeStatus omeThreadPoolFlush(omeThreadPool *tp);
+
+// pause and unpause the processing of new tasks
+// while paused, it is still possible to add tasks to the pool
+omeStatus omeThreadPoolPause(omeThreadPool *tp);
+omeStatus omeThreadPoolUnPause(omeThreadPool *tp);
+
+// return the number of tasks currently waiting
 int omeThreadPoolGetRemainingTasks(omeThreadPool *tp);
+
+// return the number of tasks currently running
 int omeThreadPoolGetRunningTheads(omeThreadPool *tp);
-void omeThreadPoolWaitForCompletion(omeThreadPool *tp);
-void *omeThreadPoolMain(void *threadPool);
 
 
 #ifdef __cplusplus
